dbfd8c4acbef3fde076f69ca368599ede2687c3b
Yun Zhou <zhou0360@umn.edu>
2018-11-11 17:05:04 -0600

    Updated the formats for main.py and README.md based on instructor's feedback

--- log-analysis-project/README.md
+++ log-analysis-project/README.md
@@ -1,28 +1,71 @@
-This is the Log Analysis project from my Full Stack Web Developer Nanodegree program at Udacity. 
 
-My task is to create a reporting tool that prints out reports (in plain text) based on the data in the database. This reporting tool is a Python program using the psycopg2 module (postgresql) to connect to the database. Please refer to the main.py file for more details. 
+**Description**
 
-Here are the questions the reporting tool should answer. The example answers given aren't the right ones, though!
+This is the Log Analysis project from my [Full Stack Web Developer](https://in.udacity.com/course/full-stack-web-developer-nanodegree--nd004) Nanodegree program at Udacity
 
-1. What are the most popular three articles of all time? Which articles have been accessed the most? Present this information as a sorted list with the most popular article at the top.
+My task is to create a reporting tool that prints out reports (in plain text) based on the data in the database. This reporting tool is a Python program using the psycopg2 module (postgresql) to connect to the database. Please refer to the main.py file for more details.
 
-Example:
+**Instructions**
 
-"Princess Shellfish Marries Prince Handsome" — 1201 views
-"Baltimore Ravens Defeat Rhode Island Shoggoths" — 915 views
-"Political Scandal Ends In Political Scandal" — 553 views
+1. Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads) and [Vagrant](https://www.vagrantup.com/)
 
-2. Who are the most popular article authors of all time? That is, when you sum up all of the articles each author has written, which authors get the most page views? Present this as a sorted list with the most popular author at the top.
+2. Clone the repository to your local host:
 
-Example:
+  ```
+  git clone https://github.com/visheshbanga/Log-Analysis-Udacity-Project
+  ```
+3. Start the virtual machine
 
-Ursula La Multa — 2304 views
-Rudolf von Treppenwitz — 1985 views
-Markoff Chaney — 1723 views
-Anonymous Contributor — 1023 views
+  In your project directory, run the following code to start the virtual machine:
 
-3. On which days did more than 1% of requests lead to errors? The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser. 
+  ```
+  vagrant up
+  ```
 
-Example:
+  When your prompt gets back, run the following code to access the virtual machine:
 
-July 29, 2016 — 2.5% errors
+  ```
+  vagrant ssh
+  ```
+
+4. Download the [data](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+
+5. Setup Database
+
+  ```
+  psql -d news -f newsdata.sql;
+  ```
+
+
+6. Run the Module
+
+  ```
+  python main.py
+  ```
+
+
+**Output**
+
+The output can be found in the output.txt file in this folder.
+
+Q1. What are the most popular three articles of all time?
+
+	1. Candidate is jerk, alleges rival -- 338647
+
+	2. Bears love berries, alleges bear -- 253801
+
+	3. Bad things gone, say good people -- 170098
+
+
+Q2. Who are the most popular article authors of all time?
+
+	1. Ursula La Multa -- 507594
+
+	2. Rudolf von Treppenwitz -- 423457
+
+	3. Anonymous Contributor -- 170098
+
+
+Q3. On which days did more than 1% of requests lead to errors?
+
+	1. Jul 17, 2016 --    2.26%
--- log-analysis-project/main.py
+++ log-analysis-project/main.py
@@ -1,74 +1,69 @@
-#Your task is to create a reporting tool that prints out reports (in plain text) 
-#based on the data in the database. This reporting tool is a Python program 
-#using the psycopg2 module to connect to the database.
+#! /usr/bin/env python3
 
 import psycopg2
 
 Q1 = 'Q1. What are the most popular three articles of all time?'
 
 query1 = """select articles.title, count(log.id) as views
-			from articles join log on log.path = concat('/article/', articles.slug)
-			group by articles.title
-			order by views desc
-			limit 3"""
+from articles join log on log.path = concat('/article/', articles.slug)
+group by articles.title
+order by views desc
+limit 3"""
 
 Q2 = 'Q2. Who are the most popular article authors of all time?'
 
-query2 = """select authors.name, count(log.id) as views
-			from log 
-			join articles on log.path = concat('/article/', articles.slug)
-			join authors on articles.author = authors.id 
-			group by authors.id
-			order by views desc 
-			limit 3"""
+query2 = """select authors.name, count(log.id) as views from log
+join articles on log.path = concat('/article/', articles.slug)
+join authors on articles.author = authors.id
+group by authors.id
+order by views desc
+limit 3"""
 
 Q3 = 'Q3. On which days did more than 1% of requests lead to errors?'
 
-query3 = """select 
-			to_char(log_date::date, 'Mon dd, yyyy') as log_date
-			, to_char(100.0*error_rate, '999D99%') as error_rate
+query3 = """select
+to_char(log_date::date, 'Mon dd, yyyy') as log_date
+, to_char(100.0*error_rate, '999D99%') as error_rate
+from
+(select
+  date_trunc('day', time) as log_date
+, sum(case when log.status like '%404%'
+then 1 else 0 end)::decimal/count(*) as error_rate
+, sum(case when log.status like '%404%' then 1 else 0 end) as errors
+, count(*) as total_requests
+from log
+group by date_trunc('day', time)
+) t
+where t.error_rate > 0.01"""
 
-			from 
-			(
-				select 
-
-				date_trunc('day', time) as log_date
-				, sum(case when log.status like '%404%' then 1 else 0 end)::decimal/count(*) as error_rate
-				--, to_char(100.0*(sum(case when log.status like '%404%' then 1 else 0 end))/count(*), '999D99%') as error_rate
-
-				, sum(case when log.status like '%404%' then 1 else 0 end) as errors
-				, count(*) as total_requests
-
-				from log  
-
-				group by date_trunc('day', time) 
-
-			) t 
-
-			where t.error_rate > 0.01"""
 
 def get_ans(Q, query):
-	# connect to the database 
-	connection = psycopg2.connect(database="news")
+    try:
+        connection = psycopg2.connect(database="news")
+    except psycopg2.DatabaseError, e:
+        print("Error: cannot connect to the database")
 
-	# 1) Declare a cursor object that defines a result set 
-	crsr = connection.cursor()
+    # 1) Declare a cursor object that defines a result set
+    crsr = connection.cursor()
 
-	# 2) Open the cursor to establish the result: return all the entries from the database
-	crsr.execute(query)
+    # 2) Open the cursor to establish the result
+    crsr.execute(query)
 
-	# 3) Fetch the data into local variables as needed from the cursor, one row at a time.
-	ans = crsr.fetchall() 
+    # 3) Fetch the data into local variables as needed from the cursor
+    ans = crsr.fetchall()
 
-	print(Q)
-	for i in range(len(ans)): 
-		print ("\n\t{}. {} -- {}".format(i+1, ans[i][0], ans[i][1]))
+    print(Q)
+    for i in range(len(ans)):
+        print("\n\t{}. {} -- {}".format(i+1, ans[i][0], ans[i][1]))
 
-	print ("\n")
-	# 4) Close the cursor when done.
-	connection.close()
+    print("\n")
+
+    # 4) Close the cursor when done.
+    connection.close()
 
 
-get_ans(Q1, query1)
-get_ans(Q2, query2)
-get_ans(Q3, query3)
+if __name__ == "__main__":
+
+    get_ans(Q1, query1)
+    get_ans(Q2, query2)
+    get_ans(Q3, query3)
